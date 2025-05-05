4d6a836a99a5dd422f8fe12e897177ce7c2937c6
muhamadjawad <jawadmansoor1998@gmail.com>
2025-05-05 09:36:44 +0500

    splash screen working

--- App.tsx
+++ App.tsx
@@ -5,12 +5,20 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { TranslatorScreen } from './src/screens/TranslatorScreen';
 import { COLORS } from './src/constants/theme';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide()
+    }, 2000);
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <TranslatorScreen />
