fd4a6ece42df7870717f4984e098706bb6ab58e5
muhamadjawad <jawadmansoor1998@gmail.com>
2025-05-31 18:19:20 +0500

    app deletgate updated for ios

--- ios/language_translator/AppDelegate.swift
+++ ios/language_translator/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import RNSplashScreen.h  
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -28,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       in: window,
       launchOptions: launchOptions
     )
+    RNSplashScreen.show()
 
     return true
   }
